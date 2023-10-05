1ae57f80e6bb23d23d40d187919003ae78dced68
Bruno Reeve <114944836+Bruno5654@users.noreply.github.com>
2023-10-05 09:10:30 +0100

    Innital Commit

---
+++ Source/CppForEngines/CppForEnginesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CppForEnginesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPFORENGINES_API ACppForEnginesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
